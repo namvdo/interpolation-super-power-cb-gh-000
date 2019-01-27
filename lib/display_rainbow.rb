01e66115019b5a4f396bfe44d547b0ac455ea8ea
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-27 10:31:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 "R: #{display_rainbow[0]}"
-end
+
